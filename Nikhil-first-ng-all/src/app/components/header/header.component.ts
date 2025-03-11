abba5b361d411209b0f395f4f08c1b057778dc4a
Nikhil Tathe <nikhil.tathe@dell.com>
2025-03-11 12:59:31 -0400

    Data binding non-signals

--- Nikhil-first-ng-all/src/app/components/header/header.component.ts
+++ Nikhil-first-ng-all/src/app/components/header/header.component.ts
@@ -7,5 +7,5 @@ import { Component, signal } from '@angular/core';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-  title = signal('Nikhil\'s first Angular app');
+  title = 'Nikhil\'s first Angular app';
 }
